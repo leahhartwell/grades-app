58ab2dd2eab0ae2acf9f03ec7c9fbade43d4d88a
Leah Hartwell <leahh222@student.ubc.ca>
2022-01-15 22:40:39 -0800

    colour blocks

--- gradesApp/components/CourseTab.js
+++ gradesApp/components/CourseTab.js
@@ -3,7 +3,17 @@ import { View, Text, StyleSheet} from 'react-native';
 
 const CourseTab = (props) => {
     return (
-        <View style={styles.CourseTabWrapper}>
+        <View style={
+            {
+                backgroundColor: props.colour,
+                opacity: 0.9,
+                padding: 20,
+                borderRadius: 10,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 15,
+            }}>
             <Text style={styles.text}>{props.course}</Text>
             <Text style={styles.text}>{props.grade}</Text>
         </View>
@@ -11,16 +21,6 @@ const CourseTab = (props) => {
 }
 
 const styles = StyleSheet.create({
-    CourseTabWrapper: {
-        backgroundColor: '#C4DADB',
-        opacity: 0.9,
-        padding: 20,
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 15,
-    },
     text: {
         maxWidth: '90%',
         fontSize: 20,
